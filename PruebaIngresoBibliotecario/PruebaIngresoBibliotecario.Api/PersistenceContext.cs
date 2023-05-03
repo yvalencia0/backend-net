4d017858a11f4f48da8594542dbfc9648793d5e9
Git Server Admin <admin@git-server>
2023-05-03 15:02:08 +0000

    Add initial repository

---
+++ PruebaIngresoBibliotecario/PruebaIngresoBibliotecario.Api/PersistenceContext.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using System.Threading.Tasks;
+
+namespace PruebaIngresoBibliotecario.Infrastructure
+{
+    public class PersistenceContext : DbContext
+    {
+
+        private readonly IConfiguration Config;
+
+        public PersistenceContext(DbContextOptions<PersistenceContext> options, IConfiguration config) : base(options)
+        {
+            Config = config;
+        }
+
+        public async Task CommitAsync()
+        {
+            await SaveChangesAsync().ConfigureAwait(false);
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.HasDefaultSchema(Config.GetValue<string>("SchemaName"));
+
+            base.OnModelCreating(modelBuilder);
+        }
+    }
+}
